77aab26fe4b72f41829a12b2dd9d246d40a2cc29
19bhartisingh <singhbharti1904@gmail.com>
2025-09-27 11:44:02 +0530

    requirements

--- template.py
+++ template.py
@@ -25,10 +25,8 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials/ipynb",
-    "templates/index.html",
+    "templates/index.html"
     
-    "requirements.txt"
-
     
 ]
 
